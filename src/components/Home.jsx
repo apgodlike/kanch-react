32353ed91bcc43b0e397f85808195d358491e50c
Godlike <apgodlike@protonmail.com>
2024-01-21 01:41:43 +0530

    home page button size modification

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,23 +7,20 @@ const Home = () => {
   return (
     <div className="">
       <div className="md:flex md:items-center md:justify-evenly gap-10">
-        {/* <div className=""> */}
         <img className="w-full h-auto md:w-1/2" src={Home_image} />
-        {/* </div> */}
         <div className="text-center py-3">
           <h2 className="text-gray-700 text-3xl py-2">Handmade Textured Art</h2>
           <p className="text-xl text-gray-600">from the Australian Costline</p>
           <div className="flex justify-center gap-10 mt-5 mb-7">
-            <Link
-              to="/shop"
-              className="bg-[#638889] w-1/2 p-2 text-sm rounded-lg hover:scale-105"
-            >
-              Shop All
+            <Link to="/shop">
+              <button className="bg-[#638889] min-w-48 p-2 text-sm rounded-lg hover:scale-105">
+                Shop All
+              </button>
             </Link>
           </div>
         </div>
       </div>
-      <div className="bg-white text-center py-10">
+      <div className="bg-[#F9EFDB] text-center py-10">
         <h2 className="text-gray-700 text-2xl py-2">
           Free shipping Australia Wide
         </h2>
